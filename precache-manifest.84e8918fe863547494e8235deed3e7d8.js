9af8fa65a2ff05a2069f4996ad8ee3307e3d33f3
Yaswanth_Kota <yaswanthsivaram.kota@gmail.com>
2020-07-02 17:31:49 +0530

    Updates

---
+++ precache-manifest.84e8918fe863547494e8235deed3e7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d18694a7ccedae57617183d840b5faf",
+    "url": "/moviesearch/index.html"
+  },
+  {
+    "revision": "4d350f812c8960debe8f",
+    "url": "/moviesearch/static/css/main.22e81881.chunk.css"
+  },
+  {
+    "revision": "aad130bb5576aa139216",
+    "url": "/moviesearch/static/js/2.aae33bb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearch/static/js/2.aae33bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d350f812c8960debe8f",
+    "url": "/moviesearch/static/js/main.947f3937.chunk.js"
+  },
+  {
+    "revision": "869b826dc1fbc1ce7cd4",
+    "url": "/moviesearch/static/js/runtime-main.552647af.js"
+  }
+]);
